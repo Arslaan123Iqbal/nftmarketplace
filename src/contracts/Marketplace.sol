d759018385a630d7e66cc6519874ed5b08d982a7
Arslan Iqbal <admin1@ArslanIqbal.local>
2022-08-23 16:47:02 +0500

    nft

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.5.0;
+ contract Marketplace{
+    string public name;
+
+    constructor() public{
+      name = 'MAI University';
+    }
+ }
